a5a2624bfdf43dd9f4bca60d9e0bf08a37394354
Devin <devinpschweiss@gmail.com>
2022-01-26 00:21:08 -0500

    makin a reset data menu thing idk

---
+++ source/options/ResetDataState.hx
@@ -0,0 +1,34 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class ResetDataState extends MusicBeatState {
+    var resetBG:FlxSprite;
+    var bf:Boyfriend;
+    var confirmButton:FlxButton;
+}
