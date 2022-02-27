449e6c00a8ede67fbd91b006e5c9351a95df7fc7
Devin503 <devinpschweiss@gmail.com>
2022-02-27 13:26:05 -0500

    believe it or not this isnt as easy as it looks

--- source/RealScreenResolutionHours.hx
+++ source/RealScreenResolutionHours.hx
@@ -33,6 +33,8 @@ class RealScreenResolutionHours extends MusicBeatState {
     var exitingMenu:Bool = false;
     var seenExplanation:Bool = false;
     var currentStep:Int = 0;
+    var updatedStep:Bool = false; // to prevent update from constantly calling doStepThings
+    var inDialogue:Bool = false;
     public static var seenMessage:Bool = false;
     public static var screenShoe:Array<Float> = [];
     var resolutionMessage:String;
@@ -161,6 +163,7 @@ class RealScreenResolutionHours extends MusicBeatState {
         }
         if (windowsThing != null) {
             windowsThing.update(elapsed);
+            if (explanimations != null) windowsThing.animation.play(explanimations[cum]);
         }
         if (explanText != null) {
             explanText.update(elapsed);
@@ -171,8 +174,19 @@ class RealScreenResolutionHours extends MusicBeatState {
                 FlxG.sound.play(Paths.sound('menuScroll'));
                 if (cum <= explanTexts.length) cum++;
             }
+            if (cum == explanTexts.length) {
+                inExplanation = false;
+                windowsThing.destroy();
+                yesThisExists.destroy();
+                explanText.destroy();
+                currentStep = 5;
+                doStepThings(currentStep);
+            }
         }
     }
+    /**actually displays the available options lmao*/
+    var optionDesc:FlxText;
+    var optionBg:FlxSprite;
     function showOptions() {
         trace('prevent compile fail');
         if (!seenExplanation) {
@@ -180,28 +194,40 @@ class RealScreenResolutionHours extends MusicBeatState {
         } else {
             currentStep = 5;
         }
+        optionBg = new FlxSprite(0).makeGraphic(FlxG.width, 26, FlxColor.fromRGB(0,0,0,175));
+        add(optionBg);
+        optionDesc = new FlxText(0,4,FlxG.width,'Option description will be displayed here');
+        optionDesc.setFormat("VCR OSD Mono", 2, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(optionDesc);
+        /* if (currentStep == 2) {
+            for (e in 0...2) {
+                var optionChoice = 'a';
+            }
+        }*/ //i need to check how options sets it up
     }
-
+    /**this should be called at the end of every step*/
     function doStepThings(step:Int) {
         switch(step) {
-            case 0:
+            case 0: // SET STEP TO 1 FOR INITIAL DIALOGUE
                 trace('bg set');
-            case 1:
+                currentStep++;
+            case 1: // DON'T DISABLE INDIALOGUE HERE!
                 trace('amogus');
                 showScreenshot();
-            case 2:
+            case 2: // GENERATE OPTIONS
                 generateOptionsInitial();
-            case 3:
+            case 3: // EXPLAIN THE SHIT
                 #if windows
                 explainTaskbarMovement();
                 #elseif linux
                 openSubState(new LinuxSmallScreen());
                 #end
-            case 4:
+            case 4: // SET THE SHIT
                 setResolutionTrick();
-            case 5:
+            case 5: // THIS IS FOR IF THE PLAYER LISTENS TO THE EXPLANATION
                 generateOptionsFinal();
         }
+        updatedStep = true;
     }
     function exitScreen() {
         trace('done');
@@ -215,20 +241,34 @@ class RealScreenResolutionHours extends MusicBeatState {
         trace('i need to set the resolution thing up elsewhere first');
     }
     /**generates the initial option menu*/
+    var optionTextArray:Array<String> = [];
     function generateOptionsInitial() {
         trace('a');
+        optionTextArray.push('Explain taskbar movement');
+        optionTextArray.push('Set the resolution trick');
+        optionTextArray.push('I\'m good.');
     }
     /**if player hasn't set the trick from here then give option to set or exit*/
     function generateOptionsFinal() {
         trace('a');
     }
     var windowsThing:FlxSprite;
-    var explanTexts:Array<String> = ["Alright, so. First things first... Click on the Action Centre icon. It'll be on your taskbar, right next to the clock.", "Now, click All settings.\n(If necessary, click Expand first)", "Once the Settings window opens, click Personalisation.", "On the left, click on Taskbar. If your window is too small, click the 3-line (menu) icon and click Taskbar.", "Where it says Taskbar location on screen, change the value to Top, Left, or Bottom.", "You can also enable the small taskbar buttons, which would give you more screen space.", "Or, you can enable auto-hide of the taskbar in desktop mode, which will bypass the need to move the taskbar at all.", "If you go for this, you can make the option appear again by hovering near the edge where your taskbar is."];
+    var explanTexts:Array<String> = [
+        "Alright, so. First things first... Click on the Action Centre icon. It'll be on your taskbar, right next to the clock.",  //0
+        "Now, click All settings.\n(If necessary, click Expand first)", //1
+        "Once the Settings window opens, click Personalisation.", //2
+        "On the left, click on Taskbar. If your window is too small, click the 3-line (menu) icon and click Taskbar.", //3
+        "Where it says Taskbar location on screen, change the value to Top, Left, or Bottom.", //4
+        "You can also enable the small taskbar buttons, which would give you more screen space.", //5
+        "Or, you can enable auto-hide of the taskbar in desktop mode, which will bypass the need to move the taskbar at all.", //6
+        "If you go for this, you can make the option appear again by hovering near the edge where your taskbar is."]; //7
+    var explanimations:Map<Int, String>;
     var inExplanation:Bool = false;
     var explanText:FlxText;
     var cum:Int = 0;
     function explainTaskbarMovement() {
         trace('gonna get screenshots');
+        inExplanation = true;
         windowsThing = new FlxSprite(0);
         windowsThing.frames = Paths.getSparrowAtlas(Paths.image('/windTaskShit'));
         windowsThing.animation.addByPrefix('init', 'desktop', 0);
@@ -241,12 +281,23 @@ class RealScreenResolutionHours extends MusicBeatState {
         windowsThing.updateHitbox();
         windowsThing.screenCenter(X);
         add(windowsThing);
+        explanimations = [
+            0 => 'init',
+            1 => 'act',
+            2 => 'settingsMain',
+            3 => 'personalMain',
+            4 => 'persTaskbar',
+            5 => 'persTaskbar',
+            6 => 'persTaskbar',
+            7 => 'persTaskbar'
+        ];
         explanText = new FlxText(0, FlxG.height - 66, FlxG.width - 150, explanTexts[cum], 24);
         explanText.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         add(explanText);
         yesThisExists = new SnowdriftIcon('snowcon');
         yesThisExists.x = FlxG.width - 150;
         yesThisExists.y = FlxG.height - 150;
+        yesThisExists.flipX = true;
         add(yesThisExists);
     }
     public function startDialogue(dialogueFile:DialogueFile, ?song:String = null):Void
@@ -258,8 +309,9 @@ class RealScreenResolutionHours extends MusicBeatState {
                 // inCutscene = true;
                 CoolUtil.precacheSound('dialogue');
                 CoolUtil.precacheSound('dialogueClose');
-                psychDialogue = new DialogueBoxPsych(dialogueFile, song);
+                psychDialogue = new DialogueBoxPsych(dialogueFile);
                 psychDialogue.scrollFactor.set();
+                inDialogue = true;
                 if(exitingMenu) {
                     psychDialogue.finishThing = function() {
                         psychDialogue = null;
@@ -267,6 +319,7 @@ class RealScreenResolutionHours extends MusicBeatState {
                     }
                 } else {
                     psychDialogue.finishThing = function() {
+                        if (currentStep != 1) inDialogue = false;
                         psychDialogue = null;
                         doStepThings(currentStep);
                     }
@@ -284,4 +337,18 @@ class RealScreenResolutionHours extends MusicBeatState {
                 }
             }
         }
+}
+
+/**temp, i will expand on this if i ever use the sauce here on linux with a smaller resolution than 1920x1080*/
+class LinuxSmallScreen extends MusicBeatSubstate {
+    var snowdrift:SnowdriftIcon;
+    var commonDEs:Array<String> = [
+        'GNOME',
+        'KDE Plasma',
+        'XFCE',
+        'MATE',
+        'Cinnamon'
+    ]; // this is partially google results and partially environments i've used in the past
+    var curDialogue:DialogueFile;
+    var yourDE:String;
 }
