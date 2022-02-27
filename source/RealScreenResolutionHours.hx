20593121a785b9340ff3e6d132b8f44ae796ac15
Devin503 <devinpschweiss@gmail.com>
2022-02-27 00:50:57 -0500

    real screen resolution hours

---
+++ source/RealScreenResolutionHours.hx
@@ -0,0 +1,17 @@
+package;
+
+import DialogueBoxPsych;
+import flixel.FlxG;
+import openfl.system.Capabilities;
+import flixel.FlxState;
+import flixel.system.FlxSound;
+import flixel.ui.FlxButton;
+import flixel.text.FlxText;
+import HealthIcon as SnowdriftIcon;
+
+using StringTools;
+
+/* hENLO it is nearly 1am as i commit this, im tired and will work on this tmr*/
+class RealScreenResolutionHours extends MusicBeatState {
+    var yesThisExists:SnowdriftIcon;
+}
