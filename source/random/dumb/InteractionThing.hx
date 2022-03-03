0f1dac802b249492850153a24a79a67360804850
Devin503 <devinpschweiss@gmail.com>
2022-03-02 23:35:58 -0500

    dear god in [[SWEDEN]] kris

---
+++ source/random/dumb/InteractionThing.hx
@@ -0,0 +1,7 @@
+package random.dumb;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import MusicBeatState;
+import DialogueBoxPsych;
+import random.oc.DevinsCharacterList;
---
+++ source/random/dumb/readme.txt
@@ -0,0 +1,1 @@
+this is gonna include random dumb things
