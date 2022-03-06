31da611063e7f9c4a64528ae242a793473a4c45f
Devin503 <devinpschweiss@gmail.com>
2022-03-05 23:40:08 -0500

    im movin the favs to a substate

---
+++ source/random/oc/CharacterFavourites.hx
@@ -0,0 +1,86 @@
+package random.oc;
+
+import haxe.Json;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import MusicBeatSubstate;
+import random.oc.DevinsCharacterList;
+import flixel.FlxG;
+import flixel.addons.ui.FlxUITabMenu;
+import flixel.addons.ui.FlxUITooltipManager;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.addons.ui.FlxUIInputText;
+
+/**This typedef contains the same vars as the original favourite vars in OCThing. Just a bit cleaner. I hope.
+@param favouriteColours A list of favourite colours.
+@param favouriteAesthetics A list of favourite aesthetics (like grunge, neon, etc.)
+@param favouriteGames A list of favourite games.
+@param favouriteMusicGenres A list of favourite genres of music.
+@param favouriteArtists A list of favourite artists.
+@since March 2022 (Emo Engine 0.1.1)*/
+typedef CharFavourites = {
+    var favouriteColours:Array<String>; // For if a character's got some favourite colours
+    var favouriteAesthetics:Array<String>; // Ditto, but for aesthetics (like grunge, neon, etc.)
+    var favouriteGames:Array<String>; // Ditto, but for games
+    var favouriteMusicGenres:Array<String>; // Ditto, but for genres of music.
+    var favouriteArtists:Array<String>; // Ditto, but for artists
+}
+
+/**A substate that allows you to modify the current values of your character's favourites. Utilizes as many of the imports above as possible.
+    @since March 2022 (Emo Engine 0.1.1)*/
+class CharaFavouriteManager extends MusicBeatSubstate {
+    public static var daFaves:CharFavourites; // maybe static might allow the value to *stay* after exit? idk
+    static final exampleFaves:String = '{
+        "favouriteColours": [
+            "blue",
+            "yellow"
+        ],
+        "favouriteAesthetics": [
+            "neon",
+            "pastel"
+        ],
+        "favouriteGames": [
+            "Animal Crossing",
+            "Mario"
+        ],
+        "favouriteMusicGenres": [
+            "Rock",
+            "Metal",
+            "Pop"
+        ],
+        "favouriteArtists": [
+            "Weezer",
+            "CupcakKe"
+        ]
+    }'; // PLACEHOLDER, USED FOR THE CONVERT FUNCTION!!
+    var colourBox:FlxUIInputText;
+    var boxAesthetic:FlxUIInputText; //box aesthetic uwu
+    var gamerBox:FlxUIInputText;
+    var genreBox:FlxUIInputText;
+    var artistBox:FlxUIInputText;
+    var transparentBlackColour:Int = 0x6900000;
+
+    public function new(curFaves:CharFavourites) {
+        super();
+        daFaves = curFaves;
+        var bg:FlxSprite = new FlxSprite(0).makeGraphic(FlxG.width, FlxG.height, transparentBlackColour);
+        bg.screenCenter();
+        bg.scrollFactor.set();
+        add(bg);
+    }
+
+    /**This allows you to convert your current variables to the typedef.*/
+    public static function convertToNewFormat(col:Array<String>, aes:Array<String>, gam:Array<String>, mus:Array<String>, art:Array<String>):CharFavourites {
+        var bean:CharFavourites;
+        bean = cast Json.parse(exampleFaves); // JUST TO HAVE A PLACEHOLDER!
+        bean.favouriteColours = col;
+        bean.favouriteAesthetics = aes;
+        bean.favouriteGames = gam;
+        bean.favouriteMusicGenres = mus;
+        bean.favouriteArtists = art;
+        trace(bean);
+        return bean;
+    }
+
+}
