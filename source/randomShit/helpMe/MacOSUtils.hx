9e3ed19a60d6b690477f0b208a10204165d10489
alphagolem <devinpschweiss@gmail.com>
2022-03-23 19:18:25 -0400

    add username var

--- source/randomShit/helpMe/MacOSUtils.hx
+++ source/randomShit/helpMe/MacOSUtils.hx
@@ -3,3 +3,17 @@ package randomShit.helpMe;
 import haxe.Exception;
 import sys.io.File;
 import sys.FileSystem;
+import Sys;
+import flixel.util.FlxTimer;
+import sys.io.Process;
+import Paths;
+
+using StringTools;
+
+/**I'm writing this on a mac! These are utilities for MacOS users.
+    @since March 2022 (Emo Engine 0.1.2)*/
+class MacOSUtils {
+    var userName = Sys.getEnv("USER"); // thank GOD this is the same in linux and macOS.
+
+    
+}
