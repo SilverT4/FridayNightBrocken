ed1e989b2dd9a9a2cf24e5fa1d7d0122075a5c54
alphagolem <devinpschweiss@gmail.com>
2022-03-23 20:16:13 -0400

    snowdrift chatter stuff

---
+++ source/randomShit/util/SnowdriftUtil.hx
@@ -0,0 +1,16 @@
+package randomShit.util;
+
+import sys.io.File as SnowFile;
+import sys.FileSystem as SnowFS;
+import Paths.snowdriftChatter as snowJson;
+import haxe.Json.parse as Snowflake;
+
+using StringTools;
+
+/**Get the fuckin shit ig
+    @since March 2022 (Emo Engine 0.1.2)*/
+class SnowdriftUtil {
+    public static function loadChatter(ChatterName:String) {
+        return Snowflake(snowJson(ChatterName)); // quick return lmao
+    }
+}
