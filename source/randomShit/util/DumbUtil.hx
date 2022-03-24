c4fa99c691d08979a61743bea73a75031b98fed6
Devin503 <devinpschweiss@gmail.com>
2022-03-24 12:56:15 -0400

    new functions

--- source/randomShit/util/DumbUtil.hx
+++ source/randomShit/util/DumbUtil.hx
@@ -1,5 +1,7 @@
 package randomShit.util;
 
+import flixel.util.FlxColor;
+import haxe.Exception;
 import PlayState;
 import Character;
 import Boyfriend;
@@ -55,17 +57,17 @@ class DumbUtil {
         var charList:Array<String> = [];
         var baseGameChars = FileSystem.readDirectory('assets/characters');
         for (i in 0...baseGameChars.length) {
-            if (getExt(baseGameChars[i]) == 'json') charList.push(baseGameChars[i]);
+            if (getExt(baseGameChars[i]) == 'json') charList.push(snipName(baseGameChars[i]));
         }
         #if MODS_ALLOWED
         var modCharBase = FileSystem.readDirectory('mods/characters');
         for (i in 0...modCharBase.length) {
-            if (getExt(modCharBase[i]) == 'json') charList.push(modCharBase[i]);
+            if (getExt(modCharBase[i]) == 'json') charList.push(snipName(modCharBase[i]));
         }
         if (Paths.currentModDirectory != '') {
             var modCharCur = FileSystem.readDirectory('mods/' + Paths.currentModDirectory + '/characters');
             for (i in 0...modCharCur.length) {
-                if (getExt(modCharCur[i]) == 'json') charList.push(modCharCur[i]);
+                if (getExt(modCharCur[i]) == 'json') charList.push(snipName(modCharCur[i]));
             }
         }
         #end
@@ -84,11 +86,55 @@ class DumbUtil {
             return benis[1];
         }
     }
+
+    /**Parse the JSON of a character without loading its sprites.
+        
+    @param JsonName the character json name
+    @returns CharacterFile variable if found
+    @throws Exception if no JSON is found
+    @since March 2022 (Emo engine 0.1.2)*/
+    public static function getCharFile(JsonName:String):Character.CharacterFile {
+        if (Paths.fileExists(Paths.characterJson(JsonName), openfl.utils.AssetType.TEXT, false)) return cast Json.parse(getRawFile(Paths.characterJson(JsonName)));
+        else throw new haxe.Exception('no file found');
+    }
+
+    /**Gets the healthbar colours of a character in FlxColor format.
+        
+    @param Colours The colour array
+    @returns An `FlxColor` object
+    @since March 2022 (Emo Engine 0.1.2)*/
+    public static function getFromRGB(Colours:Array<Int>):FlxColor {
+        return FlxColor.fromRGB(Colours[0], Colours[1], Colours[2]);
+    }
+
+    /**@returns The raw data of the file
+        @since March 2022 (Emo Engine 0.1.2)*/
+    public static function getRawFile(FilePath:String):String {
+        return sys.io.File.getContent(FilePath);
+    }
+
+    /**Gets the name of a file without its extension.
+        
+    @param FileName The file
+    @returns Its name*/
+    public static function snipName(FileName:String):String {
+        var snipper = FileName.split('.');
+        return snipper[0];
+    }
     /**Easily parse Snowdrift Chatter files.
     
     @param Chatter The file to parse.
     @returns A DialogueFile variable with the chatter stuff*/
     public static function parseSnowdriftChatter(Chatter:String):DialogueFile {
         return cast Json.parse(Chatter);
+    }
+
+    public static function doBirthdayCheck() {
+        if (TitleState.currentProfile != null) {
+            var susDate = DevinsDateStuff.getTodaysDate();
+            if (susDate[0] == TitleState.currentProfile.playerBirthday[0] && susDate[1] == TitleState.currentProfile.playerBirthday[1]) return true;
+                else return false;
+        }
+        return false;
     }
 }
