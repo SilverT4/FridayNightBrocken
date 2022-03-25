ce79b822ff38e914c848f02266ee1e9729901274
Devin503 <devinpschweiss@gmail.com>
2022-03-25 13:14:53 -0400

    add bday override to snowdrift util

--- source/randomShit/util/SnowdriftUtil.hx
+++ source/randomShit/util/SnowdriftUtil.hx
@@ -13,4 +13,6 @@ class SnowdriftUtil {
     public static function loadChatter(ChatterName:String) {
         return Snowflake(snowJson(ChatterName)); // quick return lmao
     }
+
+    public static var birthdayOverride:Bool = false; // if this is set to true then any songs played have their opponent replaced with snowdrift!
 }
