b8009c32e84943b32772db5b886d336e905bfb74
Devin503 <devinpschweiss@gmail.com>
2022-03-29 12:22:45 -0400

    this now warns on init and loads prefs

--- source/MusicBeatLauncher.hx
+++ source/MusicBeatLauncher.hx
@@ -9,17 +9,41 @@ import MusicBeatState;
     @since March 2022 (Emo Engine 0.1.2)*/
 class MusicBeatLauncher extends MusicBeatState {
     var NewState:Dynamic;
+    var hadToInit:Bool = false;
     public function new(Newstate:MusicBeatState) {
         super();
         if (PlayerSettings.player1 == null) {
+            hadToInit = true;
+            if (TitleState.currentProfile != null) {
+                flixel.FlxG.save.bind(TitleState.currentProfile.saveName, "fridayNightBrocken");
+                var dumbass = TitleState.currentProfile.profileName;
+                Achievements.achievementsStuff.push(["Happy Birthday!",				"Happy birthday, " + dumbass + "!\nCheck the options menu for a surprise.",  'birthday',	true]); // just to have this. lmao
+            } else {
+                flixel.FlxG.save.bind("funkin", "ninjamuffin99");
+            }
             PlayerSettings.init(); // JUST IN CASE!!
+            ClientPrefs.loadPrefs();
             #if debug flixel.FlxG.log.add('HEY!! Just a very quick heads up: You forgot to initalise the player settings! The launcher you called has done it for you, but please be careful in the future!'); #end
         }
         NewState = Newstate;
     }
 
     override function create() {
-        launchState(NewState);
+        if (hadToInit) {
+            var speen:flixel.FlxSprite = new flixel.FlxSprite(flixel.FlxG.width - 48, 0);
+            speen.frames = Paths.getSparrowAtlas("editor/speen", "preload");
+            speen.animation.addByPrefix("speen", "spinner go brr", 30, true);
+            speen.animation.play("speen");
+            var fuckinShit:randomShit.util.HintMessageAsset = new randomShit.util.HintMessageAsset("Hey! Quick heads up: Player settings weren't initialised. That's been done for you now by this launcher state.", 12, ClientPrefs.smallScreenFix);
+            add(fuckinShit);
+            add(fuckinShit.ADD_ME);
+            add(speen);
+            new flixel.util.FlxTimer().start(3, function(fuck:flixel.util.FlxTimer) {
+                launchState(NewState);
+            });
+        } else {
+            launchState(NewState);
+        }
     }
     public static function launchState(LaunchThis:MusicBeatState) {
         LoadingState.loadAndSwitchState(LaunchThis);
