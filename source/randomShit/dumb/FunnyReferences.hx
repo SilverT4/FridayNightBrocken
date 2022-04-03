9a992e3f187d004f7bd3a495432f7411dac00c9e
Devin503 <devinpschweiss@gmail.com>
2022-04-03 16:27:34 -0400

    GIRL YESS!!!!! I LOVE IT WHEN MR KRABS SHOVES HIS SECRET FORMULA IN MY SAFE WHERE HE CAN RELEASE THE SECRET RECIPE

--- source/randomShit/dumb/FunnyReferences.hx
+++ source/randomShit/dumb/FunnyReferences.hx
@@ -6,3 +6,42 @@ using StringTools;
 typedef SmallerThanMyToes = {
     var YeaYea:Int;
 }
+
+/**GIRL YES!! I LOVE PUTTING SPIDER EGGS UP MY POOSAY AND FEELING THEM HATCH! THEY SCRATCH MY POOSAY REALLY WELL!!
+    
+@since Whenever the fuck I found this comment. (Class available since March 2022 (Emo Engine 0.2.0)*/
+class GirlYes {
+    public static var theComments:Array<String> = [
+        'GIRL YES!!! i love smelling my cousins dirty underwear, sometimes when im lucky they even have a stain of cvm on them, they turn me on',
+        'GIRLLL YESSS!!! I LOVE FINDING SPIDER EGGS AND PUTTING THEM IN MY VGYNA!! WHEN THEY HATCH THEY ALWAYS SCRATCH THE PLACES I CANT REACH!!',
+        'GIRL YES !!!! I LOVE MIXING MY CVM WITH MY SISTERS PERIOD BLOOD!!!! LOOKS LIKE A STRAWBERRY SMOOTHIE!!',
+        'GIRL YES!! I LOVE IT WHEN I FART AND IT SMELLS SO BAD THAT FLOWERS WILT AND EVERYONE AROUND ME ALMOST DIES!',
+        'GIRL YESS!!!! MY UNCLE WAS CLIPPING HIS TOENAILS AND HE LEFT THEM ALL ON THE FLOOR AND I SUCKED THEM UP THRU MY PÜŠŠŸ LIKE A VACUUM',
+        'GIRLL YESSS MY STEPBRO CAME IN THE BATHROOM AND HE STARTED “HELPING” ME WITH THE LAUNDRY I CAN STILL FEEL HIS WHITE DETERGENT ON ME',
+        'GIRL YES!! YOU\'RE INVITED TO JIAFEI\'S HAUNTED CVM HOUSE! BRING YOUR FAMILY, YOUR OCS, HELL, JUST BRING THE ENTIRE WORLD!!',
+        'GIRL YESS!!!!! I LOVE IT WHEN MR KRABS SHOVES HIS SECRET FORMULA IN MY SAFE WHERE HE CAN RELEASE THE SECRET RECIPE',
+        'Girl yes!! My stepdad takes the huggest sh!ts and before he flushed I go and drink his diarrhea!!!',
+        'Girl YESSS!!! I LOVE USING MY GRANDMAS USED PADS AS A FACE MASK IN CLASS!! EVERYONE WANTS A SNIFF!',
+        'GIRL YES! I love smelling your dad\'s used pads, they smell like heaven. I look forward to drinking the leftover period blood every month!',
+        'Girl YESS! When I saw a huge boner on my teacher I will go secretly take his boxers and smell it like a perfume!',
+        'GIRL YESSS!!! The lunch lady kidnapped me and used a scooper and shove it on me and I cvmmed and she used my cvm for a new milk for lunch!!',
+        'GIRL YESS I LIKE WHEN GRANDPA SUCK ON MY BEEF PATTY AND THEN THE DUST GETS IN AND I LICK IT UP',
+        'GIRL YES!! MY 4 GAY UNCLES CVMED OVER TO MY HAUNTED HOUSE AND HAD SVX!!! I WASNT IN IT BUT I WATCHED AND THEY BECAME FAMOUS CORNSTARS!',
+        'Walmart starts whimpering as target goes in Walmart quietly moaning so the others don’t hear as target goes deeper and Walmart moans very loud.',
+        'Girl YES! I love it when my math teacher jerks off in class and then all the hornknee students go to lick up his cvm -- I always get the most!',
+        'Wendy begins screaming as McDonalds shoves it in. Instead of saying babababa. She began to say ahahahah. And as it says I’m lovin it shes also love it',
+        'as Russia pounded Ukraine, Ukraine smiled and said "I never knew you were this good :weary:"',
+        '*target moaned as Walmart thrusted into target* Walmart: shhhhhh we can’t let Amazon hear or you will be in bigger trouble~~~~… *as target cried*',
+        'Sonic held Mario as he continuously moaned. Sonic grinned and whispered “my sweet Mario..”',
+        'YouTube grunted pounding into tiktok, TikTok smiled “wow YouTube W for you”',
+        'that\'s not how you do it! " dominos said as Pizza Hut thrusted into her. " Pizza Hut stop~! " dominos moaned helplessly',
+        'Ohhh gooooon “sticks squidwards nose out” that power is so strong and good i wanna crush you “ gon look at hisoka up and down and runs” ohhhhhhh gon',
+        '"s-shadow please be gentle~" said the smaller blue hedgehog to the bigger alpha male dominate "heh, well see ~" said the bigger black hedgehog smirkin',
+        'Amazon starts going inside ebay very slowly but surely,Amazon groans and eBay moans,Amazon starts going crazy and eBay twerks Amazon creams everywhere',
+        'KFC puts it in deeper into McDonald\'s while mdonalds moans softly, KFC goes harder then McDonald\'s moans loudly and starts cvmming in kfc',
+        '"Ah~" said roblox as fortnite thrusts deeper "want to go slower?~" said fortnite "n-no d-daddy" says roblox as he squirts',
+        '*Roblox moans* “AHH~ MINECRAFT~ HARDER~ *minecraft goes harder* “Is that better baby?😏” “OH YES~ DADDY~ OH~ MMMM~ THIS FEELS SO GOOD~” “Roblox moans”',
+        'ahh!~ It\'s too big!~ said pillow chan as blanket san put his big juicy long blanketussy in him. Shh~ Mattress san will hear us!~ Blanket san told her.',
+        'mcdonald\'s moans “a-ahh!~” burger king shushes him “sshh~.. dont want the others too hear~” burger king goes deeper, mcdonald\'s moans loudly.'
+    ];
+}
